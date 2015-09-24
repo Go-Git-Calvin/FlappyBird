6dc047ddac472461ff27443a2a89592da76b4ec6
Calvin Wong <Calvin@CYW.local>
2015-09-24 13:39:30 -0400

    Initial commit

---
+++ src/StartFlappyBird.java
@@ -0,0 +1,25 @@
+/**
+ * Flappy Bird Game GUI
+ */
+import javax.swing.*;
+
+public class StartFlappyBird {
+
+    public static int WIDTH = 500;
+    public static int HEIGHT = 500;
+
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame();
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(WIDTH, HEIGHT);
+        frame.setLocationRelativeTo(null);
+
+        Keyboard keyboard = Keyboard.getInstance();
+        frame.addKeyListener(keyboard);
+
+        GamePanel panel = new GamePanel();
+        frame.add(panel);
+    }
+} // end of class
